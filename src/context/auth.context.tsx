ab4106542cc97d6dfaf31c22a02918bdd43e92cd
Alejandra Rodriguez Tibana <30965660+malejaroti@users.noreply.github.com>
2025-09-30 19:44:10 +0200

    fix(auth): correct token key and improve authentication logic

--- src/context/auth.context.tsx
+++ src/context/auth.context.tsx
@@ -19,7 +19,7 @@ interface AuthProviderProps {
 // context component (component that sends the state contexts and functions)
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
-const TOKEN_KEY = "auth_token";
+const TOKEN_KEY = "authToken";
 
 // wrapper component (component that holds the states and functions to be shared)
 function AuthProvider({children}: AuthProviderProps) {
@@ -27,17 +27,23 @@ function AuthProvider({children}: AuthProviderProps) {
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
   const [loggedUserId, setLoggedUserId] = useState<string | null>(null);
   const [isAdmin, setIsAdmin] = useState<boolean>(false);
-  const [isAuthenticating, setIsAuthenticating] = useState<boolean>(true);
+  const [isAuthenticating, setIsAuthenticating] = useState<boolean>(false);
   const navigate = useNavigate()
 
   useEffect(() => {
-    // at the start of the app, validate the user token
-    authenticateUser();
+    // at the start of the app, validate if there is an auth token in local storage
+    const storageToken = localStorage.getItem(TOKEN_KEY)
+    console.log("storageToken: ", storageToken)
+    if (storageToken) {
+      authenticateUser();
+    } else {
+      setIsAuthenticating(false)
+    }
   }, []);
 
   const authenticateUser = async (): Promise<void> => {
     // this is the function that sends the token to the backend to verify its validity and receives info about the owner of that token
-
+    setIsAuthenticating(true)
     try {
       const response = await api.get('/auth/verify');
 
