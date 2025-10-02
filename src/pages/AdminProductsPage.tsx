f2efd0a10604d7e91c72cb52e1628bb51a50169e
Alejandra Rodriguez Tibana <30965660+malejaroti@users.noreply.github.com>
2025-10-02 03:21:08 +0200

    refactor(AdminProductsPage): Comment out unused code and clean up component structure

--- src/pages/AdminProductsPage.tsx
+++ src/pages/AdminProductsPage.tsx
@@ -7,6 +7,7 @@ import { Typography } from "@mui/material";
 import { type Product } from "../types/Product";
 import MuiDataGrid from "../components/MuiDataGrid";
 import { DataGrid, type GridColDef } from '@mui/x-data-grid';
+import FullFeaturedCrudGrid from "../components/DataGridActions";
 
 
 
@@ -14,7 +15,7 @@ function AdminProducts() {
   const [allProducts, setAllProducts] = useState<Product[]>([]);
 
   useEffect(() => {
-    getAllProducts()
+    // getAllProducts()
   }, [])
 
   const getAllProducts = async () => {
@@ -42,34 +43,16 @@ function AdminProducts() {
     }
   }
 
-  const handleClickAddProduct = () => {
-
-  }
-
-  type ProductRow = Omit<Product, 'variants' | 'images'>
-  const columns: GridColDef<ProductRow>[] = [
-    // { field: 'id', headerName: 'ID', width: 90},
-    { field: 'name', headerName: 'Name', width: 200, editable: true },
-    { field: 'latinName', headerName: 'Latin name', width: 100 },
-    { field: 'bulkGrams', headerName: 'Lagerbestand (g)', type: 'number', width: 120 },
-    { field: 'reorderAtGrams', headerName: 'Mindestbestand (g)', type: 'number', width: 130 },
-    { field: 'originCountry', headerName: 'Herkunft', width: 110 },
-    { field: 'organicCert', headerName: 'Bio', width: 50 },
-    { field: 'slug', headerName: 'URL', width: 90, editable: false },
-    { field: 'active', headerName: 'Sichtbar im Shop', width: 90, editable: false },
-  ];
-
-  const rows = allProducts.map(({ descriptionMd, variants, images, categories, ...rest }) => rest);
-
   return (
     <PageShell>
       <Typography variant="h4"> Alle Produkte</Typography>
-      {
+      {/* {
         allProducts.map(eachProduct => (
           <Typography key={eachProduct.id}>{eachProduct.name}</Typography>
         ))
-      }
-      <MuiDataGrid columns={columns} rows={rows} />
+      } */}
+      {/* <MuiDataGrid columns={columns} rows={rows} /> */}
+      <FullFeaturedCrudGrid/>
       <Button
         variant="contained"
         component={RouterLink}
@@ -80,16 +63,9 @@ function AdminProducts() {
           right: '10px'
 
         }}
-      // onClick={handleClickAddProduct}
       > Produkt hinzufügen
       </Button>
-      <Button
-        variant="contained"
-        component={RouterLink}
-        to={`/admin/products/create`}
-      // onClick={handleClickAddProduct}
-      > Produkt bearbeiten
-      </Button>
+
     </PageShell>
   )
 }
