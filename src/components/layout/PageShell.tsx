f2fef4d06f12ab16acd55c289075e5e468c29116
Alejandra Rodriguez Tibana <30965660+malejaroti@users.noreply.github.com>
2025-10-02 12:55:40 +0200

    fix(PageShell): remove unnecessary border style for cleaner layout

--- src/components/layout/PageShell.tsx
+++ src/components/layout/PageShell.tsx
@@ -2,7 +2,7 @@ import { Box, Container, Paper } from '@mui/material';
 
 export default function PageShell({ children }: { children: React.ReactNode }) {
   return (
-    <Box sx={{ p: 1, borderRadius: 3 , minHeight: '100dvh', minWidth: '100dhp', display: 'flex', alignItems: 'center', border:1, bgcolor: 'background.default' }}>
+    <Box sx={{ p: 1, borderRadius: 3 , minHeight: '100dvh', minWidth: '100dhp', display: 'flex', alignItems: 'center', bgcolor: 'background.default' }}>
       <Container >
         <Paper sx={{ position:'relative', p: 4, borderRadius: 3 }}>
           {children}
